47ce5026b5073eaac5ae9f3cd861bf8c5372de60
rogerio.sobrinho <x216948>
2020-04-05 16:42:34 -0300

    test: ensure SignUpController return an error if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -42,4 +42,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: password'));
   });
+
+  it('should return 400 if no password is provider', () => {
+    const sut = new SignUpController();
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpResquest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'));
+  });
 });
