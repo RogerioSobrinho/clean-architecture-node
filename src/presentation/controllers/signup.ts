f91bed03b5faf2877a0935e902ed62f0e3af2edd
rogerio.sobrinho <x216948>
2020-04-05 23:24:28 -0300

    feat: ensure SignUpController return an invalid param if invalid email provider

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,14 +2,25 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../erros/missing-param-error';
 import { badRequester } from '../helpers/http-helpers';
 import { Controller } from '../protocols/controllers';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParam } from '../erros/invalid-param-error';
 
 export class SignUpController implements Controller {
-  handle (httpResquest: HttpRequest): HttpResponse {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
+  handle(httpResquest: HttpRequest): HttpResponse {
     const paramRequired = ['name', 'email', 'password', 'passwordConfirmation'];
     for (const param of paramRequired) {
       if (!httpResquest.body[param]) {
         return badRequester(new MissingParamError(param));
       }
+    }
+    if (!this.emailValidator.isValid(httpResquest.body.email)) {
+      return badRequester(new InvalidParam('email'));
     }
   }
 };
